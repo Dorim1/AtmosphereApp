e2d026994e140131c97742d61abdd31bfdd8faba
Dreamy <tolya.lyashhenko.05@mail.ru>
2026-03-15 15:57:33 +0300

    Initial commit

---
+++ app/src/main/java/ru/anlyashenko/atmosphereapp/core/designsystem/theme/Color.kt
@@ -0,0 +1,28 @@
+package ru.anlyashenko.atmosphereapp.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// LightTheme
+val BackgroundWhiteLight = Color(0xFFFAFAFC)
+val OnBackgroundBlackLight = Color(0xFF181818)
+
+val PrimaryPurple = Color(0xFF607AFF)
+val OnPrimaryWhite = Color(0xFFFFFFFF)
+
+val ContainerWhiteLight = Color(0xFFFFFFFF)
+val OnContainerGrayLight = Color(0xFFEBEBEB)
+
+val SecondaryWhiteLight = Color(0xFFFFFFFF)
+val OnSecondaryBlackLight = Color(0xFF181818)
+
+// Other
+val SecondaryDarkLight = Color(0xFF2B2B2B)
+val SecondaryGrayLight = Color(0xFFEBEBEB)
