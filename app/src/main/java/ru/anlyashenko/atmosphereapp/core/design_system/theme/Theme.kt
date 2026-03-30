0f7e0fcbf737fa156b2757ed584e7fa3beb8db81
Dreamy <tolya.lyashhenko.05@mail.ru>
2026-03-30 12:46:05 +0300

    AA-Navigation-Refactor navigation structure and implement `NavigationBar2` (nw)
    - Rename `Destinations.kt` to `Destination.kt` and update references across the project.
    - Rename `NavGraph.kt` to `AppNavHost.kt` and refactor the navigation logic into a reusable `AppNavHost` component.
    - Implement `NavigationBar2`, a new container component that manages a `Scaffold` with conditional `BottomNavigationBar` visibility based on the current destination hierarchy.
    - Update `BottomNavigationBar.kt` to use the refactored `Destination` routes and include logic for state-aware navigation item selection.
    - Update `MainActivity.kt` to use `NavigationBar2` as the root UI component and disable dynamic colors in `AtmosphereAppTheme`.
    - Enable dynamic color support and dark theme switching logic in `Theme.kt`.

--- app/src/main/java/ru/anlyashenko/atmosphereapp/core/design_system/theme/Theme.kt
+++ app/src/main/java/ru/anlyashenko/atmosphereapp/core/design_system/theme/Theme.kt
@@ -1,10 +1,14 @@
 package ru.anlyashenko.atmosphereapp.core.design_system.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -49,7 +53,7 @@ fun AtmosphereAppTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = LightColorScheme /*when {
+    val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -57,7 +61,7 @@ fun AtmosphereAppTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }*/
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
